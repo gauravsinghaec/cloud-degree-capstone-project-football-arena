20b9cb7f7c6c755754030b3d6d91cf88c4e1d090
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2023-01-05 01:25:08 +0530

    fix: new api host after correcting port number in reverseproxy

--- fa-frontend/src/config/config.js
+++ fa-frontend/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   production: false,
   appName: 'Football Arena',
-  apiHost: 'http://a5794d3a2b1fe455db0f25c3f7ea52f6-871863892.us-east-1.elb.amazonaws.com:4000/api/v0',
+  apiHost: 'http://a6cc3f63395ec488bbbaf94c45c77aaa-616625647.us-east-1.elb.amazonaws.com:4000/api/v0',
 };
 export default config;
