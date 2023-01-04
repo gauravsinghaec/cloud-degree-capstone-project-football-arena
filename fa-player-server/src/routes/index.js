0d32e19abb96277ac71488943d0c94d53a62c3fe
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2023-01-04 16:56:35 +0530

    fix: remove the redundant api endpoint

--- fa-player-server/src/routes/index.js
+++ fa-player-server/src/routes/index.js
@@ -3,11 +3,6 @@ const express = require('express');
 const router = express.Router();
 const playerRoutes = require('./api/player');
 
-/* GET home page. */
-router.get('/*', (req, res) => {
-  res.render('index');
-});
-
 // Player router
 router.use('/api', playerRoutes);
 
